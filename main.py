cb4358e506f936647f49970d245317b865fe0c48
dangquytaikb <dangquytai776617185@gmail.com>
2024-04-24 09:44:11 +0200

    Add read_json endpoint to main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 ﻿from fastapi import FastAPI
-
+import json
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# read json file
+@app.get("/data")
+async def read_json():
+    with open('systemdata.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data
+
+
+
 
