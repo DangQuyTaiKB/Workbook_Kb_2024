f9e705e0054f151a3f8fa3d5abf126adf995c722
dangquytaikb <dangquytai776617185@gmail.com>
2024-04-24 10:37:21 +0200

    excel test

--- main.py
+++ main.py
@@ -15,5 +15,29 @@ async def read_json():
     return data
 
 
+def read_josn(file_name):
+    with open(file_name , 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data
+
+def main():
+    data = read_josn('systemdata.json')
+    users_dat= data['users']
+    
+    import pandas as pd
+    df = pd.DataFrame(users_dat)
+    # print(df)
+
+    df.to_excel('users_data.xlsx', index=False)
+
+    # create pivot table
+    pivot_table = df.pivot_table(values='id', index='name', columns='surname', aggfunc='count')
+    pivot_table.to_excel('pivot_table.xlsx')
+
+if __name__ == '__main__':
+    main()
+
+
+
 
 
